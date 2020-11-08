15eb9f2f2e5e36c00fbaa0e10e7015c3a5c46396
broothie <adbooth8@gmail.com>
2020-11-08 13:00:03 -0800

    rubifying

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source "https://rubygems.org"
+
+gem "sinatra", "~> 2.1"
+gem "sinatra-contrib", "~> 2.1"
+gem "sass", "~> 3.7"
+gem "dotenv", "~> 2.7"
+gem "google-cloud-firestore", "~> 2.3"
+gem "google-cloud-storage", "~> 1.29"
+
+group :development do
+  gem "pry", "~> 0.13.1"
+  gem "byebug", "~> 11.1"
+  gem "pry-byebug", "~> 3.9"
+end
+
+gem "sinatra-flash", "~> 0.3.0"
---
+++ server.rb
@@ -0,0 +1,148 @@
+require 'dotenv/load'
+require 'sinatra'
+require 'sinatra/flash'
+require 'sinatra/reloader' if development?
+require 'sass'
+require 'google/cloud/firestore'
+require 'google/cloud/storage'
+
+enable :sessions
+
+get '/' do
+  @entries = entries.get.sort_by(&:created_at).reverse
+  title 'Home'
+  erb :index
+end
+
+get '/photos/manage' do
+  require_logged_in!
+
+  @entries = entries.get.sort_by(&:created_at).reverse
+  title 'Manage Photos'
+  erb :manage_photos
+end
+
+post '/photos' do
+  require_logged_in!
+
+  # Get uploaded photo data
+  photo = params[:photo]
+  filename = photo[:filename]
+  local_file = photo[:tempfile]
+
+  # Upload file to cloud
+  cloud_file = bucket.create_file(local_file, filename)
+
+  # Save entry to db
+  entry = entries.add(
+    title: params[:title],
+    blurb: params[:blurb],
+    filename: filename,
+    url: cloud_file.public_url,
+  )
+
+  # Show uploaded photo on homepage
+  redirect "/#photos-#{entry.document_id}"
+end
+
+get '/photos/:entry_id' do |entry_id|
+  require_logged_in!
+
+  @entry = entries.doc(entry_id).get
+  title 'Edit Photo'
+  erb :edit_photo
+end
+
+post '/photos/:entry_id' do |entry_id|
+  require_logged_in!
+
+  entries.doc(entry_id).update({
+    title: params[:title],
+    blurb: params[:blurb]
+  })
+
+  redirect "/#photos-#{entry_id}"
+end
+
+get '/login' do
+  redirect_to_callback if logged_in?
+
+  title 'Log In'
+  erb :login
+end
+
+post '/login' do
+  if password_correct?(params[:password])
+    session[:logged_in] = true
+    redirect_to_callback
+  else
+    flash[:error] = 'Incorrect password'
+    redirect '/login'
+  end
+end
+
+delete '/login' do
+  log_out!
+end
+
+get '/style.css' do
+  scss :base
+end
+
+helpers do
+  # Template
+  def title(title)
+    @title = title
+  end
+
+  # Login
+  def password_correct?(password)
+    (@password ||= ENV['PASSWORD']) == password
+  end
+
+  def log_in!
+    session[:logged_in] = true
+  end
+
+  def log_out!
+    session.delete(:logged_in)
+  end
+
+  def logged_in?
+    session[:logged_in]
+  end
+
+  def require_logged_in!
+    callback_redirect '/login' unless logged_in?
+  end
+
+  # Callbacks
+  def callback_redirect(path)
+    session[:callback] = request.path
+    redirect path
+  end
+
+  def redirect_to_callback
+    redirect session.fetch(:callback, '/')
+  end
+
+  # GCP
+  def entries
+    @entries ||= Google::Cloud::Firestore
+      .new(**gcp_args)
+      .col(settings.development? ? 'entries-development' : 'entries')
+  end
+
+  def bucket
+    @bucket ||= Google::Cloud::Storage
+      .new(**gcp_args)
+      .bucket(settings.development? ? 'deweys-photos-development' : 'deweys-photos')
+  end
+
+  def gcp_args
+    @gcp_args ||= {
+      project: 'deweys-photos',
+      keyfile: settings.development? ? 'gcloud-key.json' : nil
+    }
+  end
+end
